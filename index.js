7d4b72ab4764f5bbe91f4304c3f69cbbc86506a7
khunliang <94458459+khunliang@users.noreply.github.com>
2023-03-21 18:57:47 +0700

    multiply table

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
+let numberInput = document.getElementById('number-input');
+let runButton = document.getElementById('run-button');
+let output = document.getElementById('output');
 
+function printMultiply(){
+    let number = Number(numberInput.value);
+    let outputHtml = '';
 
-function plus (a, b) {
-  return a + b
-};
+    for(let i=1; i<=12; i++){
+      outputHtml += '<p>';
+      outputHtml += number + ' x ' + i + ' = ' + (number * i);
+      outputHtml += '</p>';
+    }
+    output.innerHTML = outputHtml;
+}
 
-plus (1, 2)
+runButton.addEventListener('click', printMultiply);
